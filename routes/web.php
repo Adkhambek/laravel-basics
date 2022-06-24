65a6e6ad0d863f12afd06470cdd2a5290b0e8a3f
Adham <muzaffarov.adham@gmail.com>
2022-06-24 19:06:09 +0500

    routes and views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/laravel', function () {
+   return view('laravel');
+});
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get("/user", function () {
+    return ["username" => "Adham", "age" => 23, "single" => true];
+});
